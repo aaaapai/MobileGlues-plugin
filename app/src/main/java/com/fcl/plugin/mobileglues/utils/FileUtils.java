65b60a3a0061ef1e8363a4632d2d52c7998516c7
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-22 00:44:20 +0800

    Update FileUtils.java

--- app/src/main/java/com/fcl/plugin/mobileglues/utils/FileUtils.java
+++ app/src/main/java/com/fcl/plugin/mobileglues/utils/FileUtils.java
@@ -1,4 +1,4 @@
-package com.fcl.plugin.mobileglues.utils;
+package com.fcl.plugin.mobileglues.ap.utils;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -113,7 +113,7 @@ public class FileUtils {
 	
 	private void deleteAppFiles() {
 		Uri mgDirUri = MainActivity.MGDirectoryUri;
-		File mgDir = new File(Environment.getExternalStorageDirectory(), "MG");
+		File mgDir = new File(Environment.getExternalStorageDirectory(), "MG_AP");
 		File config = new File(mgDir, "config.json");
 		File cache = new File(mgDir, "glsl_cache.tmp");
 		File log   = new File(mgDir, "latest.log");
