d78c3b98af8dd2dc8d803cbd457be6875ecb7596
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-13 09:40:30 +0800

    Create AppInfoDialogBuilder.kt

---
+++ app/src/main/java/com/fcl/plugin/mobileglues/AppInfoDialogBuilder.kt
@@ -0,0 +1,30 @@
+package com.fcl.plugin.mobileglues
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.core.net.toUri
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+@SuppressLint("InflateParams")
+class AppInfoDialogBuilder(context: Context) : MaterialAlertDialogBuilder(context) {
+
+    init {
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_app_info, null, false)
+        view.findViewById<TextView>(R.id.info_version).text = BuildConfig.VERSION_NAME
+
+        setTitle(R.string.dialog_info)
+        setView(view)
+        setPositiveButton(R.string.dialog_positive, null)
+        setNeutralButton(R.string.dialog_github) { _, _ ->
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    "https://github.com/aaaapai/MobileGlues".toUri()
+                )
+            )
+        }
+    }
+}
