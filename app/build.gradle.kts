cade1f44c16ac8e122a7b576fb84f1d27a65e531
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-16 02:05:43 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,5 +101,4 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.google.material)
-    implementation(project(":MobileGlues"))
 }
